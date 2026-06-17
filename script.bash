ad9e5b29884da691f4157b7664070046354ab49b
IdkTest147 <167761232+IdkTest147@users.noreply.github.com>
2026-06-17 13:37:16 +0100

    Create script.bash

---
+++ script.bash
@@ -0,0 +1,8 @@
+# Add your website files and the executable
+git add index.html installer.exe
+
+# Commit the changes
+git commit -m "Add static webpage and executable file"
+
+# Push the changes to GitHub
+git push origin main
